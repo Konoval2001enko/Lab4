64f0155791f1eafccb9a242bd5bc9cd11b65bc4b
Konoval2001enko <Konoval2001enko@outlook.com>
2021-04-25 19:36:36 +0300

    adding files

---
+++ lab-4/Models/Record.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace lab_4.Models
+{
+    public class Record
+    {
+        public int RecordID { get; set; }
+        public string Name { get; set; }
+        public string Phone { get; set; }
+
+        public Record()
+        {
+
+        }
+
+        public Record(string name, string phone)
+        {
+            this.Name = name;
+            this.Phone = phone;
+        }
+
+        public Record(int recordID, string name, string phone)
+        {
+            this.RecordID =recordID;
+            this.Name = name;
+            this.Phone = phone;
+        }
+
+        public override string ToString() => Name + " " + Phone;
+    }
+}
